fd83afe2f83b76bb2b43670a28335ceeee99a1d7
JGironB <jpgironb@unal.edu.co>
2020-04-16 16:54:52 -0500

    Añadida libreria de manipulacion de imagenes

--- Secuencial.c
+++ Secuencial.c
@@ -1,19 +1,22 @@
+//Librerias estandar de C
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-//Funcion para generar el kernel gaussiano
+//Librerias de terceros usadas para manipulacion de imágenes png, jpg, ...
+
+//Función para generar el kernel gaussiano
 void generate_kernel(int size, double** kernel) 
 { 
-    //Desviacion estandar 
+    //Desviación estándar 
     double sigma = 1.0; 
     
-    //Suma para normalizar el kernel despues 
+    //Suma para normalizar el kernel después 
     double sum = 0.0; 
     
     int mid = size/2;
 
-    //Generar sizexsize kernel usando funcion de densidad de probabilidad de Gauss
+    //Generar kernel de tamaño size x size usando funcion de densidad de probabilidad de Gauss
     for (int x = -mid; x <= mid; x++) { 
         for (int y = -mid; y <= mid; y++) { 
             kernel[x + mid][y + mid] = exp(-(x * x + y * y) / (2 * sigma * sigma)) / (2 * M_PI * sigma * sigma); 
@@ -21,7 +24,7 @@ void generate_kernel(int size, double** kernel)
         } 
     } 
   
-    //Normalizacion del kernel para un efecto mas limpio
+    //Normalización del kernel para un efecto mas limpio en el blurring
     for (int i = 0; i < size; ++i) 
         for (int j = 0; j < size; ++j) 
             kernel[i][j] /= sum; 
@@ -31,9 +34,12 @@ void generate_kernel(int size, double** kernel)
 int main(int argc, char* argv[]) {
 
     int kernel_size;
-
-    kernel_size = atoi(argv[1]);
     
+    //Extracción de tamaño del kernel 
+    kernel_size = atoi(argv[3]);
+
+
+    //Asignación dinámica de espacio para generar una matriz 
     double** kernel = (double**)malloc(sizeof(double*) * kernel_size);
     for(int i = 0; i < kernel_size; ++i) 
         kernel[i] = (double*)malloc(sizeof(double)*kernel_size);
@@ -42,13 +48,13 @@ int main(int argc, char* argv[]) {
 
     for(int i = 0; i < kernel_size; ++i) {
 
-        for(int j = 0; j < kernel_size; ++j) {
+        for(int j = 0; j < kernel_size; ++j) 
             printf("%f ", kernel[i][j]);
-        }
-
+        
         printf("\n");
     }
     
+    //Liberación de espacio usado por el kernel
     for(int i = 0; i < kernel_size; ++i) 
         free(kernel[i]);
     

3569d2672c032c04ddd706cc0d701d245c49d3ac
JGironB <jpgironb@unal.edu.co>
2020-04-17 12:17:04 -0500

    Comentarios finales

--- blur_effect.c
+++ blur_effect.c
@@ -1,16 +1,17 @@
-//Librerias estandar de C
+//Librerias estándar de C
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
 #include <sys/time.h>
 
-//Librerias de terceros usadas para manipulacion de imágenes png, jpg, etc.
+//Librerias de terceros usadas para manipulación de imágenes png, jpg, etc.
 #define STB_IMAGE_IMPLEMENTATION
-#include "lib/stb_library/stb_image.h"
+#include "stb_library/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "lib/stb_library/stb_image_write.h"
+#include "stb_library/stb_image_write.h"
 
+//Struct de parámetros pasados a los hilos para ejecutar la convolución 
 struct convolution_args {
 
     size_t sourcePixel;
@@ -27,16 +28,19 @@ struct convolution_args {
     int n_threads;
 };
 
+
+//Función que realiza la convolución dados los parametros del rango de pixeles
 void executeConvolution(struct convolution_args args){
 
+    //Construcción de tamaño de imágen que se quiere producir
     size_t img_size = args.width * args.height;
     size_t blur_channels = args.blur_channels;
     size_t blurred_image_size = img_size * blur_channels;
 
-    //unsigned char* blurred_img = (unsigned char*)malloc(sizeof(unsigned char) * blurred_image_size);
     unsigned char* blurred_img = args.blurred_img;
     unsigned char* img = args.img;
 
+    //Rango de pixeles entre los que se va a trabajar
     size_t sourcePixel = args.sourcePixel;
     size_t endPixel = args.endPixel;
 
@@ -62,28 +66,32 @@ void executeConvolution(struct convolution_args args){
 
     double** kernel = args.kernel;
 
-    
+    //Loop que itera sobre el rango de pixeles dado como argumentos
     for(; p != img+(endPixel*channels); p += channels, b_p += blur_channels, ++current_pixel) {
         
         valueRed = 0;
         valueGreen = 0;
         valueBlue = 0;
 
-        
+            //Recorrido por cada uno de los valores de la matriz del kernel
             for(int i = -mid_size; i <= mid_size; ++i){
                 for(int j = -mid_size; j <= mid_size; ++j){
+                    //Calculo de pixel de imágen original sobre el que queremos aplicar convolución
                     target_pixel = current_pixel + (i*width) + j;
 
+                    //Extracción de cada uno de los tres canales del pixel identificado
                     pixel_valueRed = target_pixel < 0 || target_pixel > width*height-1 ? 1 : *(img+(target_pixel*channels)+0);
                     pixel_valueGreen = target_pixel < 0 || target_pixel > width*height-1 ? 1 : *(img+(target_pixel*channels)+1);
                     pixel_valueBlue = target_pixel < 0 || target_pixel > width*height-1 ? 1 : *(img+(target_pixel*channels)+2);
 
+                    //Suma de valores multiplicados
                     valueRed += kernel[i+mid_size][j+mid_size] * pixel_valueRed;
                     valueGreen += kernel[i+mid_size][j+mid_size] * pixel_valueGreen;
                     valueBlue += kernel[i+mid_size][j+mid_size] * pixel_valueBlue;
                 }
             } 
 
+        //Asignación de canales modificados a la imágen con filtro
         *b_p =  (uint8_t)(valueRed);
         *(b_p + 1)=  (uint8_t)(valueGreen);
         *(b_p + 2)=  (uint8_t)(valueBlue);  
@@ -117,14 +125,17 @@ void generate_kernel(int size, double** kernel)
 } 
 
 
+//Función que asigna trabajo a cada uno de los hilos
 void *assignWork(void *args) 
 { 
     struct convolution_args * my_args = (struct convolution_args *)args;
 
     size_t img_size = my_args->width * my_args->height;
 
+    //Repartición de trabajo según blockwise
     size_t load_work = img_size/(my_args->n_threads);
 
+    //Calculamos rango de pixeles a trabajar segun el id del pixel
     my_args->sourcePixel = my_args->thread_id * load_work;
     my_args->endPixel = (my_args->thread_id+1)* load_work - 1;
 
@@ -139,9 +150,16 @@ int main(int argc, char* argv[]) {
 
     int width, height, channels;
 
+    //Verificación de cantidad de argumentos correcta
+    if(argc != 5) {
+        perror("Cantidad de argumentos no es valida!");
+        return EXIT_FAILURE;
+    }
+
     //Cargamos la imagen obteniendo sus datos
     unsigned char* img = stbi_load(argv[1], &width, &height, &channels, 0);
 
+    //Verificación de imágen válida
     if(img == NULL) {
         perror("Error cargando la imagen!\n");
         return EXIT_FAILURE;
@@ -173,6 +191,7 @@ int main(int argc, char* argv[]) {
 
     size_t blurred_image_size = width * height * 3;
 
+    //Asignación de espacio para imágen con filtro aplicado
     unsigned char* blurred_img = (unsigned char*)malloc(sizeof(unsigned char) * blurred_image_size);
 
     int n_threads = atoi(argv[3]);
@@ -183,6 +202,7 @@ int main(int argc, char* argv[]) {
 
     struct timeval start, end;
 
+    //Calculo de tiempo antes de iniciar operaciones de convolución
 	gettimeofday(&start, NULL);
 
     for (int i = 0; i < n_threads; i++) {
@@ -198,12 +218,14 @@ int main(int argc, char* argv[]) {
         args[i].n_threads = n_threads;
         args[i].thread_id = i;
 
+        //Lanzamiento de cada uno de los threads
         pthread_create(&tid[i], NULL, assignWork, (void *)&args[i]);
     } 
 
     for (int i = 0; i < n_threads; i++) 
         pthread_join(tid[i], NULL);
 
+    //Calculo de tiempo después de aplicar convolución a todos los pixeles de la imágen
     gettimeofday(&end, NULL);
     
     //Escribimos la imágen en formato jpg con el filtro aplicado
@@ -218,6 +240,10 @@ int main(int argc, char* argv[]) {
     //Liberación de espacio usado para codificación de la imágen
     stbi_image_free(img);
 
+    //Liberación de espacio usado para codificación de imágen con filtro
+    free(blurred_img);
+
+    //Tiempo total(Elapsed Wall time)
     long seconds = (end.tv_sec - start.tv_sec);
     long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
 
